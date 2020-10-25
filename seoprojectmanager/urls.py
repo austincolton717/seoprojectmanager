ca688cc656b6a72805c58b9caa2d0c46ae1b001d
Austin Colton <austincolton717@gmail.com>
2020-10-25 15:36:13 -0600

    robots.txt

--- seoprojectmanager/urls.py
+++ seoprojectmanager/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homesite.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('api/', include('api.urls')),
+    path(
+        "robots.txt",
+        TemplateView.as_view(template_name="robots.txt",
+                             content_type="text/plain"),
+    ),
 
 ]
